16aace54d9012c49bb0f9cf998397c1641bd6aea
hrach <pierre.bidkhan@gmail.com>
2021-07-25 23:41:07 -0700

    whatever

---
+++ app.js
@@ -0,0 +1,47 @@
+// let nums = [15789, 1236842, 65487, 3672, 34087, 253487, 52702];
+
+
+// nums.reduce(function (acc, next) {
+//     return Math.min(acc, next)
+// })
+
+// nums.reduce((acc, next) => {
+//     return Math.min(acc, next)
+// })
+
+
+// let sayHello = (user) => { console.log('supppp ' + user) }
+
+// let whatever = nums.map(function (values) {
+//     if (values % 2 === 0) {
+//         return 'even'
+//     } else {
+//         return 'odd'
+//     }
+// })
+
+// // TEST CODE FROM HERE ON VVVVVVV
+
+// function double(arr) {
+//     return arr.map(function (val) {
+//         return val * 2;
+//     });
+// }
+
+// let double = arr => arr.map(val => val * 2)
+
+
+// // -------------------------------------------------
+
+
+// function squareAndFindEvens(numbers) {
+//     var squares = numbers.map(function (num) {
+//         return num ** 2;
+//     });
+//     var evens = squares.filter(function (square) {
+//         return square % 2 === 0;
+//     });
+//     return evens;
+// }
+
+// let squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0)
